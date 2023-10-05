5d5654d724bc9104766920e2061c597f713fed42
Karthi Keyan <125793976+karthi200125@users.noreply.github.com>
2023-10-05 18:25:47 +0530

    give design for landing page

---
+++ client/src/components/Main/Main.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import './Main.css';
+import { BsChevronDown } from 'react-icons/bs';
+import p1 from '../../assets/p1.jpg'
+import p2 from '../../assets/p2.jpg'
+import p3 from '../../assets/p3.jpg'
+import p4 from '../../assets/p4.jpg'
+import p5 from '../../assets/p5.jpg'
+import p6 from '../../assets/p6.jpg'
+
+const Main = () => {
+  const letters = ["outfit Idea", "DIY idea", "chai time snacks idea", "Home Decor idea"];
+  const colors = ["red", "blue", "rgb(190, 190, 16)", "purple"]; 
+  const [ideaIndex, setIdeaIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIdeaIndex((prevIndex) => (prevIndex + 1) % letters.length);
+    }, 4000);
+
+    return () => {
+      clearInterval(interval); 
+    };
+  }, []);
+
+  const idea = letters[ideaIndex];
+  const color = colors[ideaIndex];
+
+  const h2Style = {
+    color: color, 
+  };
+
+  return (
+    <div className='maincon'>
+      <div className="main">
+        <h1>Get Your Next</h1>
+        <h2 style={h2Style}>{idea}</h2>
+      </div>
+      <div className="downicon">
+        <BsChevronDown size={22} />
+      </div>
+      <div className="mainimages">
+        <img src={p1} alt="" />
+        <img src={p2} alt="" />
+        <img src={p3} alt="" />
+        <img src={p4} alt="" />
+        <img src={p5} alt="" />        
+        <img src={p6} alt="" />        
+        <img src={p1} alt="" />        
+      </div>
+    </div>
+  );
+}
+
+export default Main;
--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './LandingPage.css'
 import LandingPageNavbar from '../../components/LandingPageNavbar/LandingPageNavbar'
+import Main from '../../components/Main/Main'
 
 const LandingPage = () => {
   return (
     <div className='landingpage'>
         <LandingPageNavbar/>
+        <Main/>
     </div>
   )
 }
