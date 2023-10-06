58c0ff38ffad5e84e53d446d95e28da84aac6341
Karthi Keyan <125793976+karthi200125@users.noreply.github.com>
2023-10-06 20:13:16 +0530

    add lazy laoding function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,43 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import React, { useContext } from 'react'
+import React, { lazy, useContext, Suspense } from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { AuthContext } from './Context/Authcontext'
-import Create from './pages/Create/Create'
-import EditProfile from './pages/EditProfile/EditProfile'
-import Home from './pages/Home/Home'
-import LandingPage from './pages/LandingPage/LandingPage'
-import Profile from './pages/Profile/Profile'
-import Singleimg from './pages/SingleImg/Singleimg'
-import Upload from './pages/Upload/Upload'
-import Explore from './pages/Explore/Explore'
 import { Toaster } from 'react-hot-toast';
+import Loading from './components/Loading/Loading'
 
+const Home = lazy(() => import('./pages/Home/Home'));
+const Create = lazy(() => import('./pages/Create/Create'));
+const EditProfile = lazy(() => import('./pages/EditProfile/EditProfile'));
+const Profile = lazy(() => import('./pages/Profile/Profile'));
+const Singleimg = lazy(() => import('./pages/SingleImg/Singleimg'));
+const Upload = lazy(() => import('./pages/Upload/Upload'));
+const Explore = lazy(() => import('./pages/Explore/Explore'));
+const LandingPage = lazy(() => import('./pages/LandingPage/LandingPage'));
 
 const App = () => {
 
-  const {user} = useContext(AuthContext)
+  const { user } = useContext(AuthContext)
   const queryClient = new QueryClient();
 
   return (
     <div>
       <QueryClientProvider client={queryClient}>
-        <Toaster/>
-      <BrowserRouter>      
-        <Routes>          
-          <Route path='/' element={user ? <Navigate to="/home"/> :<LandingPage/>}/> 
-          <Route path='/home' element={user ?  <Home/> : <Navigate to="/"/>} />
-          <Route path='/create' element={user ?  <Create/> : <Navigate to="/"/>} />
-          <Route path='/explore' element={user ?  <Explore/> : <Navigate to="/"/>} />
-          <Route path='/upload' element={user ?  <Upload/> : <Navigate to="/"/>} />
-          <Route path='/profile/:id' element={user ?  <Profile/> : <Navigate to="/"/>} />
-          <Route path='/single/:id' element={user ?  <Singleimg/> : <Navigate to="/"/>} />
-          <Route path='/editprofile/:id' element={user ?  <EditProfile/> : <Navigate to="/"/>} />
-        </Routes>
-      </BrowserRouter>
+        <Toaster />
+        <BrowserRouter>
+          <Suspense fallback={<Loading/>}>
+            <Routes>
+              <Route path='/' element={user ? <Navigate to="/home" /> : <LandingPage />} />
+              <Route path='/home' element={user ? <Home /> : <Navigate to="/" />} />
+              <Route path='/create' element={user ? <Create /> : <Navigate to="/" />} />
+              <Route path='/explore' element={user ? <Explore /> : <Navigate to="/" />} />
+              <Route path='/upload' element={user ? <Upload /> : <Navigate to="/" />} />
+              <Route path='/profile/:id' element={user ? <Profile /> : <Navigate to="/" />} />
+              <Route path='/single/:id' element={user ? <Singleimg /> : <Navigate to="/" />} />
+              <Route path='/editprofile/:id' element={user ? <EditProfile /> : <Navigate to="/" />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
       </QueryClientProvider>
     </div>
   )
